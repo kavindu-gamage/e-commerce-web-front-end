6bc2bf087647a7517fe9582303b23605daa9ada1
Kavindu <kavindu2308@gmail.com>
2023-09-07 11:14:00 +0530

    Initial commit for front-end

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+
+const App = () => {
+    return <ProductList/>;
+    return <Login/>;
+  };
+  
+  export default App;
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://img.freepik.com/free-photo/portrait-young-stylish-girl-model-casual-summer-clothes-brown-hat-with-natural-makeup-isolated_158538-8581.jpg?w=1380&t=st=1683816750~exp=1683817350~hmac=8bbb8f6f3098444e3fe233da070cfcd3bdb5072aae0e69b3e62b31d93618e3c9",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://img.freepik.com/free-photo/incendiary-fantastically-beautiful-girl-coat-eco-fur-moves-fun-picture-lovely-lady-pink-clothes_197531-26843.jpg?w=2000&t=st=1684172129~exp=1684172729~hmac=c8e82c335589fe27be8699d9493029d5c6cdee461ff98be2cf94e35c2cf5e926",
+      title: "WINTER COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/free-photo/young-beautiful-smiling-female-trendy-summer-skirt-top-clothes-sexy-carefree-woman-posing-near-blue-wall-studio-positive-brunette-model-having-funcheerful-happy_158538-24922.jpg?w=1800&t=st=1683833158~exp=1683833758~hmac=e1a50d093c8a4f3436f2c64ab6073e667bb45ab1b1dbf413f7f2600330fa4a7b",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://img.freepik.com/free-photo/charming-lady-fashionable-plaid-long-dress-with-white-wide-belt-posing-with-pink-bouquet-smiling-isolated-background_197531-28782.jpg?w=900&t=st=1684171454~exp=1684172054~hmac=ef89ab50b89498cf6c9e83a9bd3d5982a5ee2ead1b2b54e7ddfa305bbee49997",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://img.freepik.com/free-photo/fashionable-caucasian-brunette-girl-with-collected-hair-red-lips-wears-black-blouse-purple-background-elegant-style-concept_197531-32081.jpg?w=900&t=st=1684171293~exp=1684171893~hmac=b6094a6be8baf173f549bb4e1c43fdff70fea2bd8c68e7cb25f2c0515564316c",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/free-photo/full-length-stylish-young-caucasian-brunette-girl-wears-tshirt-jacket-jeans-pink-background-lifestyle-female-beauty-concept_197531-31591.jpg?w=900&t=st=1684171487~exp=1684172087~hmac=d8e93e6463bf3e64a2cf31a8cc9914dace803738aa80db0cdebf580c51ff5b34",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
